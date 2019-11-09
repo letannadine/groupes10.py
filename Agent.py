6a56c9100469204921d18c6f8b0a8ffda051d0a6
GARSAY HABIB SERVICE <56684398+McGarsay@users.noreply.github.com>
2019-11-09 15:02:51 +0100

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,11 +1,12 @@
 #coding:utf-8
   class Agent:
      def__init __ (self, nom, prenom, poste):
-        self.nom=""
-        self.prenom=""
-        self.poste=""
+        auto . nom = ""
+        auto . prenom  = ""
+        self . poste    =    ""
         
         
      def_str_(self):
         retourne (self.nom, self.prenom, self.poste)
+Un  séjour  sans  faille
 Un séjour sans faille
