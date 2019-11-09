9034110c6f97d5fd4a69a6ba9319d51c39176ce0
letannadine <57175198+letannadine@users.noreply.github.com>
2019-11-09 10:38:19 +0100

    Add files via upload

---
+++ compte.py
@@ -0,0 +1,37 @@
+class Agent:
+    def __unit__(self,nom,prenom):
+        self.nom = "nom"
+        self.prenom = "prenom"
+        self.poste="poste"
+    def __str__(self):
+        return "{0} {1} {3}".format("nom,prenom,poste")
+class compte(Agent):
+    def __unit__(self):
+        self.nom = "nom"
+        self.solde = 0
+        Agent.__init__(self,nom)
+clients={}
+reponse="oui"
+while reponse=="oui":
+    nom=input("quel est le nom du client ?")
+    montant=input("quel est le montant stocké par le client?")
+    type=input("quel est le type de compte sollicité?")
+    clients[nom]=compte()
+    clients[nom].nom=nom
+    clients[nom].solde=montant
+    reponse=input("voulez vous continuer avec un autre client oui/non\n")
+print("les comptes enregistrés sont:\n")
+for client in clients.values():
+    print("{} a stocké {}".format(client.nom,client.solde))
+
+
+
+
+
+
+
+
+
+
+
+
