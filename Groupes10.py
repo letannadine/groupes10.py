d4ff874556e404befe2c806211ab7e2691acff60
Wourden-3340 <57484962+Wourden-3340@users.noreply.github.com>
2019-11-10 21:59:22 +0100

    Importe class agent

--- Groupes10.py
+++ Groupes10.py
@@ -1,1 +1,10 @@
-#coding:utf-8
+#coding:utf-8class Agent:
+     def__init __ (self, nom, prenom, poste):
+        self.nom=""
+        self.prenom=""
+        self.poste=""
+        
+        
+     def_str_(self):
+        retourne (self.nom, self.prenom, self.poste)
+
